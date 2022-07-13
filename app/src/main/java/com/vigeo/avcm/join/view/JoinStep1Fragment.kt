edae4325077d2fa1e0cf3403b5e9a7efa7f82e02
race9 <09race13@gmail.com>
2022-07-13 14:31:41 +0900

    수정: 불필요한 소스 제거

--- app/src/main/java/com/vigeo/avcm/join/view/JoinStep1Fragment.kt
+++ app/src/main/java/com/vigeo/avcm/join/view/JoinStep1Fragment.kt
@@ -1,12 +1,8 @@
 package com.vigeo.avcm.join.view
 
 import androidx.fragment.app.Fragment
-import com.vigeo.avcm.databinding.ActivityJoinStep1Binding
 
 class JoinStep1Fragment : Fragment() {
 
-    private val joinStep1Binding: ActivityJoinStep1Binding by lazy {
-        ActivityJoinStep1Binding.inflate(layoutInflater)
-    }
 
 }
